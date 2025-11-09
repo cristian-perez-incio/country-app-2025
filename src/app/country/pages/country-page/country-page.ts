d17c14914e15083b139f294b8f13e63e549ed269
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-08 19:19:05 -0500

    Get country information

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './country-page.html'
 })
 export class CountryPage {
+
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
+
+  countryResource = rxResource({
+    params: () => ({ code: this.countryCode }),
+    stream: ({ params }) => {
+      return this.countryService.searchByCode(params.code);
+    }
+  });
 
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -19,14 +19,21 @@ export class CountryService {
     return this.search('name', query);
   }
 
-  private search(endpoint: string, query: string): Observable<Country[]> {
+  searchByCode(code: string): Observable<Country | undefined> {
+    return this.search('alpha', code, 0)
+      .pipe(
+        map(countries => countries.at(0))
+      );
+  }
+
+  private search(endpoint: string, query: string, due?: number): Observable<Country[]> {
     query = query.toLowerCase();
 
     return this.http
       .get<RESTCountry[]>(`${environment.restCountriesBaseUrl}/${endpoint}/${query}`)
       .pipe(
         map(CountryMapper.mapRESTCountriesToCountries),
-        delay(2000),
+        delay(due ?? 2000),
         catchError(error => {
           console.error('Error fetching', error);
           return throwError(
