448cee429c6d59cb741b4a58465618c82b0dfd50
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-09 17:05:41 -0500

    Results cache

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { environment } from '@environments/environment';
 import type { RESTCountry } from '../interfaces/rest-countries.interface';
 import type { Country } from '../interfaces/country.interface';
@@ -10,6 +10,7 @@ import { CountryMapper } from '../mappers/country.mapper';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private httpCache = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     return this.search('capital', query);
@@ -28,11 +29,17 @@ export class CountryService {
 
   private search(endpoint: string, query: string, due?: number): Observable<Country[]> {
     query = query.toLowerCase();
+    const cacheKey = `${endpoint}|${query}`;
+
+    if (this.httpCache.has(cacheKey)) {
+      return of(this.httpCache.get(cacheKey)!);
+    }
 
     return this.http
       .get<RESTCountry[]>(`${environment.restCountriesBaseUrl}/${endpoint}/${query}`)
       .pipe(
         map(CountryMapper.mapRESTCountriesToCountries),
+        tap(countries => this.httpCache.set(cacheKey, countries)),
         delay(due ?? 2000),
         catchError(error => {
           console.error('Error fetching', error);
